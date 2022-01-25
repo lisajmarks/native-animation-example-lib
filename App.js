1320a4f0b12638b1e30659e7504d137fe6bf2c79
BeakalDegefa <84756882+BeakalDegefa@users.noreply.github.com>
2022-01-25 14:35:12 -0500

    added the button and animation

--- App.js
+++ App.js
@@ -11,6 +11,7 @@ import Linda from "./Components/Linda/Linda";
 import Sergut from "./Components/Sergut/Sergut";
 import Lisa from "./Components/Lisa/Lisa";
 import Yiley from "./Components/Yiley/Yiley";
+import Beakal from "./Components/Beakal/Beakal";
 
 const Stack = createNativeStackNavigator();
 
@@ -25,6 +26,7 @@ function App() {
         <Stack.Screen name="Sergut" component={Sergut} />
         <Stack.Screen name="Lisa" component={Lisa} />
         <Stack.Screen name="Yiley" component={Yiley} />
+        <Stack.Screen name="Beakal" component={Beakal} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- Components/Home/Home.js
+++ Components/Home/Home.js
@@ -11,6 +11,7 @@ const Home = ({ navigation }) => {
       <AnimatedButton navigation={navigation} place="Sergut" />
       <AnimatedButton navigation={navigation} place="Lisa" />
       <AnimatedButton navigation={navigation} place="Yiley" />
+      <AnimatedButton navigation={navigation} place="Beakal" />
     </View>
   );
 };
